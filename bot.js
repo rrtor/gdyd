32d49d32c5830e3d44c7d535f8d74024c05f517c
rrtor <41769454+rrtor@users.noreply.github.com>
2018-08-06 08:11:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -270,4 +270,24 @@ if (message.content.startsWith(adminprefix + 'setT')) {
       message.channel.send({embed});
 	 }
 	});
+
+client.on('ready', function(){    
+    var ms = 40000 ;    
+    var setGame = ['$مساعدة'];    
+    var i = -1;    
+    var j = 0;    
+    setInterval(function (){    
+        if( i == -1 ){    
+j = 1;    
+       }    
+        if( i == (setGame.length)-1 ){    
+            j = -1;    
+      }    
+       i = i+j;    
+        client.user.setGame(setGame[i],`http://www.youtube.com/gg`);    
+}, ms);    
+    
+});
+
+
 client.login(process.env.BOT_TOKEN);
